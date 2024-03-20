0740fc5debd47ae94d5ddae094d2cb57aec17c95
unknown <junho4379@gmail.com>
2024-03-20 21:52:43 +0900

    account

--- api/src/main/java/com/von/api/account/Account.java
+++ api/src/main/java/com/von/api/account/Account.java
@@ -3,13 +3,19 @@ package com.von.api.account;
 import lombok.*;
 
 import java.time.LocalDate;
+import jakarta.persistence.*;
 
+
+@Entity(name = "account")
 @NoArgsConstructor (access = AccessLevel.PROTECTED)
 @Getter
 @ToString (exclude = {"id"})
 
 public class Account {
 
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String accountNumber;
     private String accountHolder;
