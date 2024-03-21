965404fc200863e34a1f5f3cd754805eb9148dcb
NohTaeHo1 <160007694+NohTaeHo1@users.noreply.github.com>
2024-03-21 18:46:04 +0900

    240321

--- api/src/main/java/com/von/api/article/Article.java
+++ api/src/main/java/com/von/api/article/Article.java
@@ -1,5 +1,14 @@
 package com.von.api.article;
 
+import com.von.api.board.Board;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,12 +17,31 @@ import lombok.ToString;
 @NoArgsConstructor
 @Getter
 @ToString(exclude = {"id"})
+@Entity(name = "article")
 public class Article {
-    private Long id;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Id
+    @Column(name = "id")
+    private Long id; // primary key
+    
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Board Board_id;
+
+    @Column(name = "TITLE")
     private String title;
+
+    @Column(name = "CONTENT")
     private String content;
+
+    @Column(name = "WRITER")
     private String writer;
+
+    @Column(name = "REGISTERDATE")
     private String registerDate;
+
     @Builder(builderMethodName = "builder")
     public Article(Long id, String title, String content, String writer, String registerDate) {
         this.id = id;
--- api/src/main/java/com/von/api/board/Board.java
+++ api/src/main/java/com/von/api/board/Board.java
@@ -1,17 +1,37 @@
 package com.von.api.board;
 
-import lombok.Builder;
+import java.util.List;
+
+import com.von.api.article.Article;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(exclude = {"id"})
+@Entity(name = "board")
 public class Board {
 
-    private Long id;
-    private String boardName;
-    private String boardType;
+    @GeneratedValue(strategy = GenerationType.AUTO)
 
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @OneToMany(mappedBy = "Board")
+    private List<Article> articles;
+
+    @Column(name = "BOARDNAME")
+    private String boardName;
+    @Column(name = "BOARDTYPE")
+    private String boardType;
+    
 
 
     @Builder(builderMethodName = "builder")
